e21aa11edc730f66565bf409b2e0f43b92caf008
fengkidding <56989531+fengkidding@users.noreply.github.com>
2019-12-25 10:28:07 +0800

    配置中心

---
+++ src/main/java/com/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author f
+ * @date 2019-11-13
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
